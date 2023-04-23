e57d671dcf0a6b56133efa58f2f8320750c0a774
radek.jancik@sunamo.cz <radek.jancik@sunamo.cz>
2023-04-23 16:27:16 +0200

    Knowledge is power.

---
+++ SunamoRuleset/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using SunamoCode;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Xml.Linq;
+global using System;

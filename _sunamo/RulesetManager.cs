d6a0404f454cd483236c58adf5ed3d083cfa92ec
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-28 09:44:38 +0100

    Buildable, removed unused internal methods in _sunamo

--- _sunamo/RulesetManager.cs
+++ _sunamo/RulesetManager.cs
@@ -17,26 +17,6 @@ internal class XmlGenerator //: IXmlGenerator
     internal StringBuilder sb = new StringBuilder();
     private bool _useStack = false;
     private Stack<string> _stack = null;
-    internal int Length()
-    {
-        return sb.Length;
-    }
-    internal void WriteNonPairTagWith2Attrs(string p1, string p2, string p3, string p4, string p5)
-    {
-        sb.AppendFormat("<{0} {1}=\"{2}\" {3}=\"{4}\" />", p1, p2, p3, p4, p5);
-    }
-    internal void WriteNonPairTagWithAttr(string p1, string p2, string p3)
-    {
-        sb.AppendFormat("<{0} {1}=\"{2}\" />", p1, p2, p3);
-    }
-    internal void Insert(int index, string text)
-    {
-        sb.Insert(index, text);
-    }
-    internal void AppendLine()
-    {
-        sb.AppendLine();
-    }
     internal XmlGenerator() : this(false)
     {
     }
@@ -47,18 +27,6 @@ internal class XmlGenerator //: IXmlGenerator
         {
             _stack = new Stack<string>();
         }
-    }
-    internal void StartComment()
-    {
-        sb.Append("<!--");
-    }
-    internal void EndComment()
-    {
-        sb.Append("-->");
-    }
-    internal void WriteNonPairTagWithAttrs(string tag, List<string> args)
-    {
-        WriteNonPairTagWithAttrs(tag, args.ToArray());
     }
     internal void WriteNonPairTagWithAttrs(string tag, params string[] args)
     {
@@ -71,26 +39,6 @@ internal class XmlGenerator //: IXmlGenerator
         }
         sb.Append(" />");
     }
-    internal void WriteCData(string innerCData)
-    {
-        WriteRaw(/*string.Format("<![CDATA[{0}]]>", innerCData)*/ string.Format("<![CDATA[{0}]]>", innerCData));
-    }
-    internal void WriteTagWithAttr(string tag, string atribut, string hodnota, bool skipEmptyOrNull = false)
-    {
-        if (skipEmptyOrNull)
-        {
-            if (string.IsNullOrWhiteSpace(hodnota))
-            {
-                return;
-            }
-        }
-        string r = /*string.Format*/ string.Format("<{0} {1}=\"{2}\">", tag, atribut, hodnota);
-        if (_useStack)
-        {
-            _stack.Push(r);
-        }
-        sb.Append(r);
-    }
     internal void WriteRaw(string p)
     {
         sb.Append(p);
@@ -99,22 +47,9 @@ internal class XmlGenerator //: IXmlGenerator
     {
         sb.AppendFormat("</{0}>", p);
     }
-    internal void WriteTag(string p)
-    {
-        string r = $"<{p}>";
-        if (_useStack)
-        {
-            _stack.Push(r);
-        }
-        sb.Append(r);
-    }
     public override string ToString()
     {
         return sb.ToString();
-    }
-    internal void WriteTagWithAttrs(string p, List<string> p_2)
-    {
-        WriteTagWithAttrs(p, p_2.ToArray());
     }
     /// <summary>
     /// if will be sth null, wont be writing
@@ -134,61 +69,13 @@ internal class XmlGenerator //: IXmlGenerator
     {
         WriteTagWithAttrs(true, nameTag, DictionaryHelper.GetListStringFromDictionary(p).ToArray());
     }
-    /// <summary>
-    /// Dont write attr with null
-    /// </summary>
-    /// <param name="p"></param>
-    /// <param name="p_2"></param>
-    internal void WriteTagWithAttrsCheckNull(string p, params string[] p_2)
-    {
-        WriteTagWithAttrs(false, p, p_2);
-    }
-    internal void WriteTagNamespaceManager(object rss, XmlNamespaceManager nsmgr, string v1, string v2)
-    {
-        ThrowEx.NotImplementedMethod();
-    }
-    bool IsNulledOrEmpty(string s)
+        bool IsNulledOrEmpty(string s)
     {
         if (string.IsNullOrEmpty(s) || s == "(null)")
         {
             return true;
         }
         return false;
-    }
-    internal void WriteTagNamespaceManager(string nameTag, XmlNamespaceManager nsmgr, params string[] args)
-    {
-        //List<string> p = new List<string>(args);
-        var p = XHelper.XmlNamespaces(nsmgr, true);
-        for (int i = 0; i < args.Count(); i++)
-        {
-            p.Add(args[i], args[++i]);
-        }
-        WriteTagWithAttrs(nameTag, p);
-    }
-    internal void WriteNonPairTagWithAttrs(bool appendNull, string p, params string[] p_2)
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendFormat("<{0} ", p);
-        for (int i = 0; i < p_2.Length; i++)
-        {
-            var attr = p_2[i];
-            var val = p_2[++i];
-            if (string.IsNullOrEmpty(val) && appendNull || !string.IsNullOrEmpty(val))
-            {
-                if (!IsNulledOrEmpty(attr) && appendNull || !IsNulledOrEmpty(val))
-                {
-                    sb.AppendFormat("{0}=\"{1}\" ", attr, val);
-                }
-            }
-        }
-        sb.Append(" /");
-        sb.Append("<");
-        string r = sb.ToString();
-        if (_useStack)
-        {
-            _stack.Push(r);
-        }
-        this.sb.Append(r);
     }
     /// <summary>
     /// if will be sth null, wont be writing
@@ -219,25 +106,8 @@ internal class XmlGenerator //: IXmlGenerator
         }
         this.sb.Append(r);
     }
-    internal void WriteElement(string nazev, string inner)
-    {
-        sb.AppendFormat("<{0}>{1}</{0}>", nazev, inner);
-    }
     internal void WriteXmlDeclaration()
     {
         sb.Append(XmlTemplates.xml);
-    }
-    internal void WriteTagWith2Attrs(string p, string p_2, string p_3, string p_4, string p_5)
-    {
-        string r = /*string.Format*/ string.Format("<{0} {1}=\"{2}\" {3}=\"{4}\">", p, p_2, p_3, p_4, p_5);
-        if (_useStack)
-        {
-            _stack.Push(r);
-        }
-        sb.Append(r);
-    }
-    internal void WriteNonPairTag(string p)
-    {
-        sb.AppendFormat("<{0} />", p);
     }
 }
